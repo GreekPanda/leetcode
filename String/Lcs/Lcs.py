354edce0e1a626a8a7fc5266e202565636b32c61
GreekPanda <271799648@qq.com>
2017-03-14 12:10:12 +0800

    将双轴快速的排序的源代码加入，相当精彩

--- String/Lcs/Lcs.py
+++ String/Lcs/Lcs.py
@@ -1,6 +1,6 @@
 class LCS:
     def longestCommonSubstring(self, s, t):
-        if not (s and t)
+        if not (s and t):
             return 0
         
         lcs = 0
@@ -11,4 +11,22 @@ class LCS:
                     lcs_temp += 1
                 if lcs_temp > lcs:
                     lcs_temp = lcs
-         return lcs;      
+         return lcs;  
+         
+         
+     def longestCommonSubstring1(self, s, t):
+        if not (s and t):
+            return 0
+            
+            
+        n, m = len(s), len(t)
+        k = [[0 for i range(m + 1)] for j ranage(n + 1)]
+        for i in range(len(s)):
+            for j in range(len(t)):
+                if s[i] == t[j]:
+                    k[i][j] = 1 + k[i][j]
+                    
+        lcs = max(map(max, k))
+        return lcs
+                    
+                   
