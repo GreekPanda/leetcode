820c77b64248f3316f7ea9c0b3978c3be3ebdcf7
GreekPanda <271799648@qq.com>
2017-03-27 09:14:20 +0800

    使用首尾指针实现

--- Integer/TwoSum/TwoSum.cc
+++ Integer/TwoSum/TwoSum.cc
@@ -20,4 +20,37 @@ class TwoSum {
 			
 			return result;
 		}
+		
+		vector<int> twoSum(vector<int> &nums, int target, int flag) {
+			vector<int> result;
+			const int len = nums.size();
+			
+			if(nums == null || len == 0)
+				return result;
+			
+			vector pair<int, int> num_index(len);
+			
+			for(int i = 0; i < len; ++i) {
+				num_index.first(nums[i]);
+				num_index.second(i);				
+			}
+			
+			sort(num_index.begin(), num_index.end());
+			int start = 0, end = len - 1;
+			
+			while(start < end) {
+				if(num_index[start].first + num_index[end].first > target) {
+					--end;
+				} else if((num_index[start].first + num_index[end].first == target) {
+					int min_index = min(nums[start].second, nums[end].second);
+					int max_index = max(nums[start].second, nums[end].second);
+					result.push_back(min_index);
+					result.push_back(max_index);
+					return result;
+				} else
+					start++;
+			}
+			
+			return result;
+		}
 };
