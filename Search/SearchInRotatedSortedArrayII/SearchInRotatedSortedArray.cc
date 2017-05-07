4815dcced478fb6e7dbc9d168108f31c82b5215b
廖黄河 <liaohuanghe@liaohuanghedeMacBook-Air.local>
2017-05-07 10:59:13 +0800

    查找目标元素是否存在反转之后有序数组中，在原数组中存在重复的元素

---
+++ Search/SearchInRotatedSortedArrayII/SearchInRotatedSortedArray.cc
@@ -0,0 +1,39 @@
+class Solution {
+    public: 
+        int searchInRotatedSortedArray(vector<int> &nums, int target) {
+            if(nums.isEmtpy())
+                return -1;
+
+            int start = 0;
+            int end = nums.size() - 1;
+            int mid = 0;
+
+            while(start + 1 < end) {
+                mid = start + (end - start) / 2;
+                if (target == nums[mid])
+                    return mid;
+
+                if (nums[start] < nums[mid]) {
+                    if (nums[start] <= target && target < nums[mid])
+                        end = mid;
+                    else
+                        start = mid; 
+                } else if(nums[start] > nums[mid]) {
+                    if (nums[mid] < target && target <= nums[end])
+                        start = mid;
+                    else
+                        end = mid;
+                } else 
+                    start++;
+
+            }
+
+            if (nums[start] == target)
+                return start;
+            if (nums[end] == target)
+                return end;
+
+            return -1
+
+        }
+};
