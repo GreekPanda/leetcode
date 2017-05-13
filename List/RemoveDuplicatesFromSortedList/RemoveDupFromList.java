7afceb405b930e5b42e5936767c8abde758715f5
GreekPanda <271799648@qq.com>
2017-05-13 22:26:44 +0800

    添加实现代码

--- List/RemoveDuplicatesFromSortedList/RemoveDupFromList.java
+++ List/RemoveDuplicatesFromSortedList/RemoveDupFromList.java
@@ -1,3 +1,26 @@
-class Solution:
-    def rmDup:
+/*
+class ListNode {
+    private int val;
+    private ListNode next;
+    ListNode(int vale) {
+        this.val = val;
+        this.next = next;
+    }
+}
+*/
+
+
+class Solution {
+    public ListNode rmDup(ListNode head) {
+        ListNode curr = head;
+        while(curr != null) {
+            while(!curr.next && curr.next.val == curr.val) {
+                curr.next = curr.next.next; 
+            } 
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
 
--- List/RemoveDuplicatesFromSortedList/RemoveDupFromList.py
+++ List/RemoveDuplicatesFromSortedList/RemoveDupFromList.py
@@ -1,3 +1,17 @@
+"""
+Link List defition:
+class ListNode(object):
+    def __init__(self, val, next=none):
+        self.val = val
+        self.next = next
+"""
 class Solution:
-    def rmDup:
+    def rmDup(self, head):
+        curr = head
+        while curr:
+            while curr.next and curr.next.val == curr.val:
+                curr.next = curr.next.next
+            curr = curr.next
+
+        return head
 
